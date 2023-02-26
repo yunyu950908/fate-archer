116988c2376723ff6e188378cea0fc4b09643102
liyu <liyu@iftech.io>
2023-02-26 21:58:13 +0800

    feat: api routes

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Blueprint
 from chatgpt_client import ChatGPTClient
 from auth_client import AuthClient
 
 # Create a new Flask application
 app = Flask(__name__)
 
-# Define a route and a view function
+api_v1 = Blueprint("api_v1", __name__, url_prefix="/api/v1")
+
+client = ChatGPTClient()
+
+
+def resp_success(data=None):
+    return jsonify({"success": True, "data": data})
 
 
 @app.route("/")
@@ -13,7 +19,7 @@ def hello():
     return "Hello, World!"
 
 
-@app.route("/api/ask", methods=["POST"])
+@api_v1.route("/ask", methods=["POST"])
 def ask():
     data = request.get_json()
 
@@ -21,18 +27,47 @@ def ask():
     conversation_id = data.get("conversation_id")
     parent_id = data.get("parent_id")
 
-    client = ChatGPTClient()
     response = client.ask(prompt, conversation_id, parent_id)
 
     return jsonify(response)
 
 
-@app.route("/api/auth", methods=["POST"])
+@api_v1.route("/auth", methods=["POST"])
 def auth():
     AuthClient().start()
-    return jsonify({"success": True})
+    return resp_success()
 
+
+@api_v1.route("/conversations", methods=["GET"])
+def get_conversations():
+    offset = request.args.get("offset")
+    limit = request.args.get("limit")
+
+    conversations = client.chatbot.get_conversations(offset, limit)
+    return resp_success(conversations)
+
+
+@api_v1.route("/messages", methods=["GET"])
+def get_msg_history():
+    conversation_id = request.args.get("conversation_id")
+    messages = client.chatbot.get_msg_history(convo_id=conversation_id)
+    return resp_success(messages)
+
+
+def gen_title():
+    pass
+
+
+@api_v1.route("/conversation", methods=["DELETE"])
+def delete_conversation():
+    conversation_id = request.args.get("conversation_id")
+    client.chatbot.delete_conversation(convo_id=conversation_id)
+    return resp_success()
+
+
+app.register_blueprint(api_v1)
 
 # Run the application
 if __name__ == "__main__":
+    print(app.url_map)
     app.run()
